f77f5cefc8cd3630c7ac871d808d3de37a1cb4d6
Dalibor Dragojevic <daco.dragojevic@gmail.com>
2018-05-05 13:10:35 +0200

    remove unneeded styling from navigation bar

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -14,10 +14,5 @@
 
 		<?php the_custom_logo(); ?>
 
-		
-		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
-		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
-	<?php endif; ?>
-
 	</div><!-- .wrap -->
 </div><!-- .site-branding -->
